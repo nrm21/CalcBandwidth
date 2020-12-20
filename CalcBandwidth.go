a62dbf15aec06b89411cb51d690cd378c1f365d8
Nathan Morrison <natemorrison@gmail.com>
2020-12-20 02:48:57 -0500

    Turned into GUI program using lxn/walk, removed config.yml dependancy, many changes

--- CalcBandwidth.go
+++ CalcBandwidth.go
@@ -2,30 +2,13 @@ package main
 
 import (
 	"fmt"
-	"locallibs/support"
 	"math"
-	"runtime"
+	"strconv"
 	"time"
 
-	"gopkg.in/yaml.v2"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 )
-
-type config struct {
-	// var name has to be uppercase here or it won't work
-	BandwidthTotal float64 `yaml:"bandwidthTotal"`
-	BandwidthLeft  float64 `yaml:"bandwidthLeft"`
-}
-
-// GetConfigContents Unmarshals the config contents from file into memory
-func GetConfigContents(filename string) config {
-	var conf config
-	err := yaml.Unmarshal(support.ReadConfigFileContents(filename), &conf)
-	if err != nil {
-		fmt.Printf("There was an error decoding the yaml file. err = %s\n", err)
-	}
-
-	return conf
-}
 
 // CalcMonthDays returns the number of days in the month
 func CalcMonthDays(month time.Month, year int) float64 {
@@ -55,18 +38,12 @@ func CalcMonthDays(month time.Month, year int) float64 {
 	return days
 }
 
-func main() {
-	if runtime.GOOS == "windows" {
-		support.SetupCloseHandler() // ctrl + c handler
-	}
-
-	config := GetConfigContents("config.yml")
-
+func calculate(bwCurrentUsed float64) string {
 	currentYear := time.Now().UTC().Year()
 	currentMonth := time.Now().UTC().Month()
 	totalDaysInMonth := CalcMonthDays(currentMonth, currentYear)
-	bwLimitGBs := config.BandwidthTotal
-	bwCurrentUsed := config.BandwidthLeft
+	var bwLimitGBs float64
+	bwLimitGBs = 1229
 	// find the number of days since the first of the month (excluding today)
 	hoursSince := time.Since(time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, time.UTC)).Hours()
 
@@ -77,16 +54,56 @@ func main() {
 	daysLeftInMonth := totalDaysInMonth - (hoursSince / 24)
 	gbPerDayLeft := gbLeftToUse / daysLeftInMonth
 
-	fmt.Println()
-	fmt.Printf("Fractional days left in month:              %.3f   (Days this month:   %d)\n", daysLeftInMonth, int(totalDaysInMonth))
-	fmt.Printf("Cumulative bandwidth allowed up to today:   %.0f GB   (Used / Left:   %.0f / %d GB)\n", gbAllowedSoFar, bwCurrentUsed, int(gbLeftToUse))
-	fmt.Printf("Bandwidth per day remaining:                %.2f GB   (Daily average:   %.2f GB)\n\n", gbPerDayLeft, gbPerDay)
+	output := fmt.Sprintf("Fractional days left in month:                      %.3f       (Days this month:  %d)\r\n", daysLeftInMonth, int(totalDaysInMonth))
+	output += fmt.Sprintf("Cumulative bandwidth allowed up to today:  %.0f GB     (Used / Left:  %.0f / %d GB)\r\n", gbAllowedSoFar, bwCurrentUsed, int(gbLeftToUse))
+	output += fmt.Sprintf("Bandwidth per day remaining:                     %.2f GB  (Daily average:  %.2f GB)\r\n", gbPerDayLeft, gbPerDay)
 
-	if runtime.GOOS == "windows" {
-		fmt.Printf("Press ctrl + c to exit\n")
+	return output
+}
 
-		// now run until user exits with ctrl + c  this allows command line to
-		// remain open as long as user likes (will close after being open an hour)
-		time.Sleep(time.Hour)
-	}
+func main() {
+	var bwCurrentUsed float64
+	bwCurrentUsed = 1
+	output := calculate(bwCurrentUsed)
+
+	var resultMsgBox *walk.TextEdit
+	var bwTextBox *walk.LineEdit
+
+	MainWindow{
+		Title:  "Bandwidth Calculator",
+		Size:   Size{800, 200},
+		Layout: VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					Label{Text: "Bandwidth Used:"},
+					LineEdit{AssignTo: &bwTextBox, Text: strconv.FormatFloat(bwCurrentUsed, 'f', -1, 64)},
+					PushButton{
+						MaxSize: Size{30, 20},
+						Text:    "Press to calculate",
+						OnClicked: func() {
+							go func() {
+								bwCurrentUsed, _ = strconv.ParseFloat(bwTextBox.Text(), 64)
+								resultMsgBox.SetText(calculate(bwCurrentUsed))
+							}()
+						},
+					},
+				},
+			},
+			HSplitter{
+				Children: []Widget{
+					TextEdit{
+						AssignTo: &resultMsgBox,
+						ReadOnly: true,
+						Font: Font{
+							Family:    "Ariel",
+							PointSize: 15,
+							//Bold:      true,
+						},
+						Text: output,
+					},
+				},
+			},
+		},
+	}.Run()
 }
