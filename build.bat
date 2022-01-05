bdfabd7ee06d6d4c0e1eb8747be28832712632ea
nrm21 <natemorrison@gmail.com>
2022-01-05 17:42:28 -0500

    changed structure of dir tree

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-go build -tags walk_use_cgo -ldflags="-H windowsgui" -o "./bin/CalcBandwidth.new" .
+go build -tags walk_use_cgo -ldflags="-H windowsgui" -o "./bin/CalcBandwidth.new" ./src
 move /y .\bin\CalcBandwidth.exe .\bin\CalcBandwidth.exe.bak
 move /y .\bin\CalcBandwidth.new .\bin\CalcBandwidth.exe
