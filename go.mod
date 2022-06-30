78e04315bf989c49f4cf8d381012e85b2ce34dca
nrm21 <natemorrison@gmail.com>
2022-06-30 00:09:25 -0400

    Added etcd support, added myetcd class

--- go.mod
+++ go.mod
@@ -1,10 +1,29 @@
-module CalcBandwidth
+module _nate/CalcBandwidth
 
-go 1.16
+go 1.17
 
 require (
+	github.com/coreos/etcd v3.3.27+incompatible // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
+	github.com/etcd-io/etcd v3.3.27+incompatible
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e
+	github.com/nrm21/support v0.0.0-20220614204806-ce131bd49875
+	go.etcd.io/etcd v3.3.27+incompatible
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.26.0
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- src/CalcBandwidth.go
+++ src/CalcBandwidth.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"math"
+	"_nate/CalcBandwidth/src/myetcd"
+	"os"
 	"strconv"
-	"strings"
-	"time"
 
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
@@ -22,6 +19,7 @@ const initialWinWidth = 975
 const initialWinHeight = 175
 
 // Global pointers
+var doEtcd bool
 var mainWin *walk.MainWindow
 var resultMsgBox *walk.TextEdit
 var bwTextBox *walk.LineEdit
@@ -29,133 +27,33 @@ var pushButton *walk.PushButton
 var key *registry.Key
 var bwCurrentUsed, prevBwCurrentUsed, daysLeftInMonth, prevDaysLeftInMonth, prevBwAtProgStart, prevDaysAtProgStart *float64
 
-// Returns the number of days in the month
-func calcMonthDays(month time.Month, year int) float64 {
-	var days float64
-
-	switch int(month) {
-	case 1, 3, 5, 7, 8, 10, 12: // january march may july august october december
-		days = 31
-	default: // other months
-		days = 30
-	}
-	if month == 2 { // february
-		days = 28
-
-		if year%4 == 0 {
-			days = 29 // february leap year
-		}
-	}
-
-	return days
-}
-
-// This does all the calculations that are shown on the screen and returns the string to be printed
-func calculateBandwidth() string {
-	currentYear := time.Now().UTC().Year()
-	currentMonth := time.Now().UTC().Month()
-	totalDaysInMonth := calcMonthDays(currentMonth, currentYear)
-	var bwLimitGBs float64
-	bwLimitGBs = 1229
-	// find the number of days since the first of the month (excluding today)
-	hoursSinceMonthStart := time.Since(time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, time.UTC)).Hours()
-
-	gbPerDay := bwLimitGBs / totalDaysInMonth
-	gbAllowedSoFar := math.Round(bwLimitGBs / totalDaysInMonth * (hoursSinceMonthStart / 24))
-	gbLeftToUse := bwLimitGBs - *bwCurrentUsed
-	*daysLeftInMonth = totalDaysInMonth - (hoursSinceMonthStart / 24)
-	gbPerDayLeft := gbLeftToUse / *daysLeftInMonth
-	bwDifferential := gbAllowedSoFar - *bwCurrentUsed
-
-	// find estimate for usage based on bw diff and time between last run
-	// bwDifference := *bwCurrentUsed - *prevBwAtProgStart
-	// timeSinceFraction := *prevDaysLeftInMonth - *daysLeftInMonth
-
-	// find out how many hours and minutes since we last ran the program
-	// minsSincePrev := int(1440 * (*prevDaysAtProgStart - *daysLeftInMonth))
-	// hoursSincePrev := (minsSincePrev - (minsSincePrev % 60)) / 60
-	// minsSincePrev = minsSincePrev % 60
-
-	// now determine if showing the estimate is worth it, it's higly inaccurate if under 144 minutes (.1 of a day)
-	// dailyUsageSincePrev := bwDifference / timeSinceFraction
-	// strDailyUsageSincePrev := ""
-	// if timeSinceFraction > .1 {
-	// 	strDailyUsageSincePrev = fmt.Sprintf("%.1f GB", dailyUsageSincePrev)
-	// } else {
-	// 	strDailyUsageSincePrev = "N/A      "
-	// }
-
-	output := fmt.Sprintf("Fractional days left in month:      %.3f         (Days this month:  %d)\r\n", *daysLeftInMonth, int(totalDaysInMonth))
-	output += fmt.Sprintf("Bandwidth allowed up to today:   %.0f GB        (Used / Differential / Left:  %.0f / %.0f / %d GB)\r\n",
-		gbAllowedSoFar, *bwCurrentUsed, bwDifferential, int(gbLeftToUse))
-	output += fmt.Sprintf("Bandwidth per day remaining:     %.3f GB  (Daily average:  %.3f GB)\r\n", gbPerDayLeft, gbPerDay)
-	// output += fmt.Sprintf("Previous Bandwidth Difference and Days since:  %.0f GB        %.3f\r\n", bwDifference, timeSinceFraction)
-	// output += fmt.Sprintf("Usage estimate since last time:  %s        (%d hours %d minutes ago)\r\n", strDailyUsageSincePrev, hoursSincePrev, minsSincePrev)
-
-	return output
-}
-
-// Calculates and outputs the necessary text to populate the main window
-func setToRegAndCalc() {
-	var err error
-	numWithoutSpace := strings.TrimSpace(bwTextBox.Text())
-	*bwCurrentUsed, err = strconv.ParseFloat(numWithoutSpace, 64)
-	if err != nil {
-		log.Println("Invalid characters detected, please use integers only")
-	} else {
-		resultMsgBox.SetText(calculateBandwidth())
-	}
-}
-
-// Attempts to read last known values of program from registry (stored from last run)
-func getRegKeyValues() registry.Key {
-	// attempt to create key (won't delete if existing)
-	k, exists, err := registry.CreateKey(registry.CURRENT_USER, regKeyBranch, registry.QUERY_VALUE|registry.SET_VALUE)
-	if err != nil {
-		log.Fatalf("Error creating registry key, exiting program")
-	}
-	if exists {
-		log.Println("Registry key already existed")
-	}
-
-	return k
-}
-
-// Attempts to write a value to a registry string
-func setSingleRegKeyValue(regName, regValue string) {
-	err := key.SetStringValue(regName, regValue)
-	if err != nil {
-		log.Fatalf("Error writing registry value %s, exiting program", regName)
-	}
-}
-
-// Things to perform just before exit
-func closingFunctions() {
-	setSingleRegKeyValue(regValue1, fmt.Sprintf("%.0f", *bwCurrentUsed))
-	setSingleRegKeyValue(regValue2, fmt.Sprintf("%.0f", *prevBwAtProgStart))
-	setSingleRegKeyValue(regValue3, fmt.Sprintf("%.4f", *daysLeftInMonth))
-	setSingleRegKeyValue(regValue4, fmt.Sprintf("%.4f", *prevDaysAtProgStart))
-}
-
-// Get a string value from the registry
-func GetRegStringValue(regStr string) string {
-	value, _, err := key.GetStringValue(regStr)
-	if err != nil {
-		log.Fatalf("Error reading registry value %s, exiting program", regStr)
-	}
-
-	return value
-}
-
 func main() {
-	key = new(registry.Key)
-	*key = getRegKeyValues()
-
+	var config Config
 	bwCurrentUsed, prevBwCurrentUsed, daysLeftInMonth, prevDaysLeftInMonth = new(float64), new(float64), new(float64), new(float64)
-	*bwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue1), 64)
-	*prevBwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue2), 64)
-	*daysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue3), 64)
-	*prevDaysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue4), 64)
+	doEtcd = false
+
+	if testSockConnect("10.150.30.18", "2379") {
+		doEtcd = true
+
+		exePath, _ := os.Getwd()
+		if exePath[len(exePath)-4:] == "\\src" || exePath[len(exePath)-4:] == "\\bin" {
+			exePath = exePath[:len(exePath)-4]
+		}
+
+		config, _ = getConfigContentsFromYaml(exePath + "\\config.yml")
+		etcdValues, _ := myetcd.ReadFromEtcd(&config.Etcd.CertPath, &config.Etcd.Endpoints, config.Etcd.BaseKeyToWrite)
+		*bwCurrentUsed, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue1], 64)
+		*prevBwCurrentUsed, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue2], 64)
+		*daysLeftInMonth, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue3], 64)
+		*prevDaysLeftInMonth, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue4], 64)
+	} else {
+		key = new(registry.Key)
+		*key = getRegKeyValues()
+		*bwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue1), 64)
+		*prevBwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue2), 64)
+		*daysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue3), 64)
+		*prevDaysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue4), 64)
+	}
 
 	// record previous settings now since these values might change a few times at runtime
 	prevBwAtProgStart, prevDaysAtProgStart = new(float64), new(float64)
@@ -219,5 +117,5 @@ func main() {
 		},
 	}.Run()
 
-	closingFunctions()
+	closingFunctions(&config)
 }
