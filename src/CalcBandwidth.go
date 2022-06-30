7dc929dea6381f47e574ff8a0eee693c8a5dbf0f
nrm21 <natemorrison@gmail.com>
2022-06-30 14:05:23 -0400

    Changed 'bandwidth used' and 'days left in month' pointers to objects

--- src/CalcBandwidth.go
+++ src/CalcBandwidth.go
@@ -25,11 +25,11 @@ var resultMsgBox *walk.TextEdit
 var bwTextBox *walk.LineEdit
 var pushButton *walk.PushButton
 var key *registry.Key
-var bwCurrentUsed, prevBwCurrentUsed, daysLeftInMonth, prevDaysLeftInMonth, prevBwAtProgStart, prevDaysAtProgStart *float64
+var bwCurrentUsed, prevBwCurrentUsed, daysLeftInMonth,
+	prevDaysLeftInMonth, prevBwAtProgStart, prevDaysAtProgStart float64
 
 func main() {
 	var config Config
-	bwCurrentUsed, prevBwCurrentUsed, daysLeftInMonth, prevDaysLeftInMonth = new(float64), new(float64), new(float64), new(float64)
 	doEtcd = false
 
 	if testSockConnect("10.150.30.18", "2379") {
@@ -42,23 +42,22 @@ func main() {
 
 		config, _ = getConfigContentsFromYaml(exePath + "\\config.yml")
 		etcdValues, _ := myetcd.ReadFromEtcd(&config.Etcd.CertPath, &config.Etcd.Endpoints, config.Etcd.BaseKeyToWrite)
-		*bwCurrentUsed, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue1], 64)
-		*prevBwCurrentUsed, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue2], 64)
-		*daysLeftInMonth, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue3], 64)
-		*prevDaysLeftInMonth, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue4], 64)
+		bwCurrentUsed, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue1], 64)
+		prevBwCurrentUsed, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue2], 64)
+		daysLeftInMonth, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue3], 64)
+		prevDaysLeftInMonth, _ = strconv.ParseFloat(etcdValues[config.Etcd.BaseKeyToWrite+"/"+regValue4], 64)
 	} else {
 		key = new(registry.Key)
 		*key = getRegKeyValues()
-		*bwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue1), 64)
-		*prevBwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue2), 64)
-		*daysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue3), 64)
-		*prevDaysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue4), 64)
+		bwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue1), 64)
+		prevBwCurrentUsed, _ = strconv.ParseFloat(GetRegStringValue(regValue2), 64)
+		daysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue3), 64)
+		prevDaysLeftInMonth, _ = strconv.ParseFloat(GetRegStringValue(regValue4), 64)
 	}
 
 	// record previous settings now since these values might change a few times at runtime
-	prevBwAtProgStart, prevDaysAtProgStart = new(float64), new(float64)
-	*prevBwAtProgStart = *bwCurrentUsed     // copy value of pointer into new pointer, NOT point to the same object (since that num will change)
-	*prevDaysAtProgStart = *daysLeftInMonth // copy value of pointer into new pointer, NOT point to the same object (since that num will change)
+	prevBwAtProgStart = bwCurrentUsed
+	prevDaysAtProgStart = daysLeftInMonth
 
 	MainWindow{
 		AssignTo: &mainWin,
@@ -79,7 +78,7 @@ func main() {
 							},
 							LineEdit{
 								AssignTo: &bwTextBox,
-								Text:     strconv.FormatFloat(*bwCurrentUsed, 'f', -1, 64),
+								Text:     strconv.FormatFloat(bwCurrentUsed, 'f', -1, 64),
 								OnKeyPress: func(keystroke walk.Key) {
 									if keystroke >= walk.Key0 && keystroke <= walk.Key9 { // if a digit key pressed
 										go setToRegAndCalc()
