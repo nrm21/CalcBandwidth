95d110c87c27c504c18a644120f648bb5d5e3526
nrm21 <natemorrison@gmail.com>
2023-10-07 15:57:36 -0400

    Added some tests

--- cmd/general_test.go
+++ cmd/general_test.go
@@ -6,25 +6,24 @@ import (
 	"time"
 )
 
-func TestGetConfigContentsFromYaml(t *testing.T) {
+func TestGeneral(t *testing.T) {
 	mw := new(MainWin)
 
-	t.Run("Get config from empty path", func(t *testing.T) {
+	t.Run("GetConfigContentsFromYaml: Get config from empty path", func(t *testing.T) {
 		expected := ""
 		if err := mw.getConfigContentsFromYaml(""); err == nil {
 			t.Errorf("ERROR: Expected: an fs.PathError but got none")
 		}
 		if expected != mw.config.Etcd.BaseKeyToWrite {
-			t.Errorf("ERROR: Expected: %q got: %q", expected, mw.config)
+			t.Errorf("ERROR: Expected: %q got: %q", expected, mw.config.Etcd.BaseKeyToWrite)
 		}
 	})
 
-	t.Run("Get config from correct YAML path", func(t *testing.T) {
-		type testresults struct {
+	t.Run("GetConfigContentsFromYaml: Get config from correct YAML path", func(t *testing.T) {
+		tests := []struct {
 			field    string
 			expected string
-		}
-		tests := []testresults{
+		}{
 			{"Endpoints", "10.150.30.17:2379"},
 			{"BaseKeyToWrite", "/nate/CalcBandwidth"},
 			{"CertPath", "E:\\Documents\\_Nate\\Computer Related\\Private keys\\Etcd Certs"},
@@ -59,24 +58,47 @@ func TestGetConfigContentsFromYaml(t *testing.T) {
 func TestCalcMonthDays(t *testing.T) {
 	mw := new(MainWin)
 
-	type testresults struct {
-		InputMonth time.Month
-		InputYear  int
-		Expected   float64
-	}
-	tests := []testresults{
-		{12, 2022, 31},
-		{2, 2022, 28},
-		{2, 2024, 29},
-		{6, 2022, 30},
+	tests := []struct {
+		name     string
+		month    time.Month
+		year     int
+		expected float64
+	}{
+		{"Check 31 days in Dec", 12, 2023, 31},
+		{"Check 28 days in Feb", 2, 2023, 28},
+		{"Check 29 days in Feb of leap year", 2, 2024, 29},
+		{"Check 30 days in Jun", 6, 2023, 30},
+		{"Check negative month", -6, 2023, 30},
+		{"Check negative year", 6, -2023, 30},
 	}
 
 	for _, test := range tests {
-		results := mw.calcMonthDays(test.InputMonth, test.InputYear)
-		if results != test.Expected {
-			t.Errorf("ERROR: Expected: %f got: %f", test.Expected, results)
-		} else {
-			t.Logf("PASS: Got: %f", results)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			results := mw.calcMonthDays(test.month, test.year)
+			if results != test.expected {
+				t.Errorf("ERROR: Expected: %f got: %f", test.expected, results)
+			}
+		})
+	}
+}
+
+func TestGetMinAndMaxOf(t *testing.T) {
+	mw := new(MainWin)
+
+	tests := []struct {
+		name   string
+		array  []float64
+		expMin float64
+		expMax float64
+	}{
+		{"Check normal array", []float64{1.456, 6345.6546, 8.324525}, 1.456, 6345.6546},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			min, max := mw.getMinAndMaxOf(test.array)
+			if min != test.expMin && max != test.expMax {
+				t.Errorf("ERROR: Expected: %f, %f got: %f, %f", min, max, test.expMin, test.expMax)
+			}
+		})
 	}
 }
